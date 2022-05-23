9d062c7186d18fb0b25c5a3a8f04d1d5e4b4ebb0
carlos260800 <mash.x2003@gmail.com>
2022-05-23 13:57:59 -0500

    include del header

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MySQL CRUD</title>
+    <!-- bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/a9afa26052.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+</nav>
