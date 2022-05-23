b78ca426ed72c2cc8aa51020a1a1dbe564601db8
absolute8red <gmorinuriosteguimail.com>
2022-05-23 13:46:41 -0500

    Archivo para editar base de datos

---
+++ edit.php
@@ -0,0 +1,53 @@
+<?php
+    include("db.php");
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $query = "SELECT * FROM task WHERE id = $id";
+        $result = mysqli_query($conn, $query);
+
+        if(mysqli_num_rows($result) == 1){
+            $row = mysqli_fetch_array($result);
+            $title = $row['title'];
+            $description = $row['description'];
+        }
+    }
+    if(isset($_POST['update'])){
+        $id = $_GET['id'];
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+
+        $query = "UPDATE task set title = '$title', description = '$description' WHERE id = $id";
+        mysqli_query($conn, $query);
+
+        $_SESSION['message'] = 'Task updated succesfully';
+        $_SESSION['message_type'] = 'dark';
+        header("Location: index.php");
+    }
+?>
+
+<?php include("includes/header.php")?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="card card-body">
+                <form action="edit.php?id=<?php echo $_GET['id']; ?>" method="POST" class="d-grid gap-3">
+                    <div class="form-group">
+                        <input type="text" name="title" value="<?php echo $title; ?>" class="form-control" placeholder="Update Title">
+                    </div>
+                    <div class="form-group">
+                        <textarea name="description"  rows="2" class="form-control" placeholder="Update description"><?php echo $description; ?></textarea>
+                    </div>
+                    <div>
+                        <button class="btn btn-success" name="update">
+                            Update
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php")?>
